88c8bd7676d69c3cfbdcd140bfa452780c48deb9
wangyang962996947 <35387750+wangyang962996947@users.noreply.github.com>
2026-04-10 16:09:04 +0800

    feat: src/App.vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <CodeScanner />
+</template>
+
+<script setup lang="ts">
+import CodeScanner from './CodeScanner.vue'
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  background: #f3f4f6;
+  min-height: 100vh;
+  padding: 24px;
+}
+</style>
